b1d8a3c62c835b7ce4812bb3df94b9107320e9da
lucasb58 <mrlucasblessing@gmail.com>
2024-12-13 14:07:32 -0800

    base is done

--- webapp.py
+++ webapp.py
@@ -1,10 +1,57 @@
 from flask import Flask, url_for, render_template, request 
 from markupsafe import Markup
+import pymongo
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+#from flask_oauthlib.contrib.apps import github #import to make requests to GitHub's OAuth
+
+import pprint
+import os
 
 import os
 import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+app.debug = True #Change this to False for production
+
+app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+oauth = OAuth(app)
+oauth.init_app(app) #initialize the app to be able to make requests for user information
+#Set up GitHub as OAuth provider
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
+    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',  
+    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+)
+connection_string = os.environ["MONGO_CONNECTION_STRING"]
+db_name = os.environ["MONGO_DBNAME"]
+
+client = pymongo.MongoClient(connection_string)
+db = client[db_name]
+collection = db['Posts'] #1. put the name of your collection in the quotes
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+#context processors run before templates are rendered and add variable(s) to the template's context
+#context processors must return a dictionary 
+#this context processor adds the variable logged_in to the conext for all templates
+@app.context_processor
+def inject_logged_in():
+    is_logged_in = 'github_token' in session #this will be true if the token is in the session and false otherwise
+    return {"logged_in":is_logged_in}
 
 #render home page
 @app.route("/")
@@ -137,6 +184,14 @@ def get_avg_verbal_score(state):
     print(verbalavgscore)
     return verbalavgscore           
 
+@app.route('/googleb4c3aeedcc2dd103.html')
+def render_google_verification():
+    return render_template('googleb4c3aeedcc2dd103.html')
+
+#the tokengetter is automatically called to check who is logged in.
+@github.tokengetter
+def get_github_oauth_token():
+    return session['github_token']
 
 if __name__=="__main__":
     app.run(debug=True)
