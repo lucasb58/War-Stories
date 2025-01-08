34e8c6f8f7e322f3ae76996fdb2fd501bc6fe389
lucasb58 <mrlucasblessing@gmail.com>
2025-01-08 12:12:50 -0800

    Fixed navbar and internal server error

---
+++ . env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export SECRET_KEY=Gundam
+export GITHUB_CLIENT_ID=Ov23likleg4r8mdDwIUB
+export GITHUB_CLIENT_SECRET=6e039e70158b8475b3a1cb029685e018f8f8b084
+export MONGO_DBNAME=WarDB
+export MONGO_CONNECTION_STRING=mongodb+srv://mrlucasblessing:9qMu55uSJ7z2cKuD@cluster0.u9xfw.mongodb.net/?retryWrites=true
+
